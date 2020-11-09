a823d5ce74f6d7374e4be444d54cd387023b4624
Akshita Kukreja <akshita.kukreja@cygrp.com>
2020-11-09 20:00:01 +0530

    Added css comments

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
     return document.getElementById(id);
   }
 
+//getting the element by Id
   var card = $('card'),
       openB = $('open'),
       closeB = $('close'),
@@ -12,15 +13,19 @@
   console.log('wat', card);
 
   if(card){
+    //when open button is clicked, open-half class is added
+    //open half and then after 1 second, open the left side of the card to add animation
     openB.addEventListener('click', function () {
     card.setAttribute('class', 'open-half');
     if (timer) clearTimeout(timer);
     timer = setTimeout(function () {
       card.setAttribute('class', 'open-fully');
       timer = null;
-    }, 1000);
+    }, 1000); //delay of 1 second
   });
 
+
+//close one flap first and then the other after a delay
   closeB.addEventListener('click', function () {
     card.setAttribute('class', 'close-half');
     if (timer) clearTimerout(timer);
